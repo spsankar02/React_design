44607e53a7be0ff9a59a07be45baed5bff75691c
spsankar02 <159914816+spsankar02@users.noreply.github.com>
2024-05-10 18:56:52 +0530

    Add files via upload

--- src/Services/BillingService.js
+++ src/Services/BillingService.js
@@ -5,6 +5,22 @@ const BILLING_API_BASE_URL = "http://localhost:8080/api/v1";
 const BillingService={
     createBilling:function(billing) {
         return axios.post(BILLING_API_BASE_URL +"/createuser", billing);
-      } 
+      } ,
+
+    getalluser:function(){
+      return axios.get(BILLING_API_BASE_URL +"/getalluserslist");
+    },
+
+    getuserbyid:function(id){
+      return axios.get(BILLING_API_BASE_URL+"/getuserbyid/"+id)
+    },
+
+    deleteuser:function(data){
+      return axios.post(BILLING_API_BASE_URL +"/deleteuser/", data)
+    },
+
+    updateuser:function(user,userid){
+      return axios.post(BILLING_API_BASE_URL+"/updateuser/",userid,user)
+    }
 }
 export default BillingService
