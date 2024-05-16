0c89bdace05dff08aeeeff0496d7e45558add591
spsankar02 <159914816+spsankar02@users.noreply.github.com>
2024-05-16 19:34:20 +0530

    Add files via upload

--- src/Services/BillingService.js
+++ src/Services/BillingService.js
@@ -2,25 +2,42 @@ import axios from "axios";
 
 const BILLING_API_BASE_URL = "http://localhost:8080/api/v1";
 
-const BillingService={
-    createBilling:function(billing) {
-        return axios.post(BILLING_API_BASE_URL +"/createuser", billing);
-      } ,
+const BillingService = {
+  createBilling: function (billing) {
+    return axios.post(BILLING_API_BASE_URL + "/createuser", billing);
+  },
 
-    getalluser:function(){
-      return axios.get(BILLING_API_BASE_URL +"/getalluserslist");
-    },
+  getalluser: function () {
+    return axios.get(BILLING_API_BASE_URL + "/getalluserslist");
+  },
 
-    getuserbyid:function(id){
-      return axios.get(BILLING_API_BASE_URL+"/getuserbyid/"+id)
-    },
+  getuserbyid: function (id) {
+    return axios.get(BILLING_API_BASE_URL + "/getuserbyid/" + id)
+  },
 
-    deleteuser:function(data){
-      return axios.post(BILLING_API_BASE_URL +"/deleteuser/", data)
-    },
+  deleteuser: function (data) {
+    return axios.post(BILLING_API_BASE_URL + "/deleteuser/", data)
+  },
 
-    updateuser:function(user,userid){
-      return axios.post(BILLING_API_BASE_URL+"/updateuser/",userid,user)
-    }
+  updateuser: function (user) {
+    return axios.post(BILLING_API_BASE_URL + "/updateuser/" ,user)
+  },
+
+  createproduct: function(data){
+    return axios.post(BILLING_API_BASE_URL+"/addproductdetails",data)
+  },
+
+  getallproduct: function(){
+    return axios.get(BILLING_API_BASE_URL+"/retrieveproductdetails")
+  },
+  deleteproduct: function(data){
+    return axios.post(BILLING_API_BASE_URL+"/deleteproductdetails/",data)
+  },
+  getProductById: function(data){
+    return axios.get(BILLING_API_BASE_URL+"/getproductbyid/"+data)
+  },
+  updateproduct: function(data){
+    return axios.post(BILLING_API_BASE_URL+"/updateproductdetails/",data)
+  }
 }
 export default BillingService
