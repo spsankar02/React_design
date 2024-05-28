b491ac121fea442a3dccf409fb254d6b3eaee916
Gomathisankar <159914816+spsankar02@users.noreply.github.com>
2024-05-28 19:26:39 +0530

    Add files via upload

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ export default function Header(){
     return(
         <div className="p-2 ms-2 me-2 mt-2 mb-1 rounded " style={{position:'sticky',backgroundColor: '#8477d9',height:'15vh'}}>
 
-            <h2 className="mt-3" style={{textAlign:'center', fontFamily:"Times New Roman"}}><b>BILLING SOFTWARE</b></h2>
+            <h2 className="mt-3" style={{textAlign:'center',fontWeight:'bolder', fontFamily:"Times New Roman"}}><b>BILLING SOFTWARE</b></h2>
         </div>
     )
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,7 +7,7 @@ export default function Home() {
     return (
         <div className='App'>
             <Header />
-            <div className='row p-2 ms-2 me-2 mb-2 rounded overflow-auto' style={{ height: '86vh' ,backgroundColor:'#e6e6e6'}}>
+            <div className='row p-2 ms-2 me-2 mb-2 rounded overflow-auto' style={{ height: '84vh' ,backgroundColor:'#f4ebff'}}>
              <Sidebar />            
 
                 <div className='col-10 p-3 rounded ' style={{marginLeft:'218px'}} >
